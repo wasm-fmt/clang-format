0ff2fce75893c1c160a45c0f146e3cb96c86783b
magic-akari <akari.ccino@gmail.com>
2023-08-21 01:47:09 +0800

    chore: add github ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ cd third_party/llvm-project
 rm -rf build
 mkdir build
 cd build
-emcmake cmake -E env CXXFLAGS="-Os -lembind -fno-rtti -DEMSCRIPTEN_HAS_UNBOUND_TYPE_NAMES=0" cmake -G Ninja -DCMAKE_BUILD_TYPE=MinSizeRel -DLLVM_ENABLE_PROJECTS=clang ../llvm
+emcmake cmake -E env CXXFLAGS="-Os -lembind -fno-rtti -DEMSCRIPTEN_HAS_UNBOUND_TYPE_NAMES=0" cmake -G Ninja -DCMAKE_BUILD_TYPE=MinSizeRel -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) ../llvm
 ninja clang-format
 
 $project_root/scripts/gen-clang-format.js $PWD/bin/clang-format.js $project_root/src/template.js $project_root/npm/clang-format.js
