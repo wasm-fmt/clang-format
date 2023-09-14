7ad4d56fec7018a842d24db5f80160c7c3c46cf9
magic-akari <akari.ccino@gmail.com>
2023-09-14 20:22:48 +0800

    fix: move to out-of-tree build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,47 +1,21 @@
 cd $(dirname $0)/..
 project_root=$(pwd)
 
-rm -rf npm
-mkdir npm
-
-cp third_party/llvm-project/clang/tools/clang-format/ClangFormat.cpp third_party/llvm-project/clang/tools/clang-format/ClangFormat.cpp.backup
-
-cp src/lib.cc third_party/llvm-project/clang/tools/clang-format/ClangFormat.cpp
-cd third_party/llvm-project
-rm -rf build
-mkdir build
+rm -rf npm build
+mkdir npm build
 cd build
 
-EXTRA_COMPILER_FLAGS="-Os -DEMSCRIPTEN_HAS_UNBOUND_TYPE_NAMES=0"
-
-EXTRA_EXE_LINKER_FLAGS="-lembind \
-                        -fno-rtti \
-                        -sMINIMAL_RUNTIME \
-                        -sNO_DYNAMIC_EXECUTION \
-                        -sNO_FILESYSTEM \
-                        -sMODULARIZE \
-                        -sENVIRONMENT=web \
-                        -sIMPORTED_MEMORY \
-                        -sALLOW_MEMORY_GROWTH \
-                        --pre-js=$project_root/src/pre.js"
-
-emcmake cmake -G Ninja -DCMAKE_BUILD_TYPE=MinSizeRel \
-    -DLLVM_ENABLE_PROJECTS=clang \
+emcmake cmake -G Ninja \
     -DCMAKE_C_COMPILER=$(which clang) \
     -DCMAKE_CXX_COMPILER=$(which clang++) \
-    -DCMAKE_C_FLAGS="${EXTRA_COMPILER_FLAGS}" \
-    -DCMAKE_CXX_FLAGS="${EXTRA_COMPILER_FLAGS}" \
-    -DCMAKE_EXE_LINKER_FLAGS="${EXTRA_EXE_LINKER_FLAGS}" \
-    ../llvm
-ninja clang-format
-
-sed 's/output.instance.exports/(output.instance||output).exports/' bin/clang-format.js >bin/clang-format-mod.js
-cp bin/clang-format.wasm $project_root/npm
-npm exec terser -- $project_root/src/template.js bin/clang-format-mod.js --config-file $project_root/.terser.json --output $project_root/npm/clang-format.js
+    ..
+ninja clang-format-wasm
 
 cd $project_root
+
+sed 's/output.instance.exports/(output.instance||output).exports/' build/clang-format-wasm.js >build/clang-format.js
+cp build/clang-format-wasm.wasm npm/clang-format.wasm
+npm exec terser -- src/template.js build/clang-format.js --config-file .terser.json --output npm/clang-format.js
+
 cp src/clang-format.d.ts src/vite.js npm
 cp package.json LICENSE README.md .npmignore npm
-
-cd $project_root
-mv third_party/llvm-project/clang/tools/clang-format/ClangFormat.cpp.backup third_party/llvm-project/clang/tools/clang-format/ClangFormat.cpp
