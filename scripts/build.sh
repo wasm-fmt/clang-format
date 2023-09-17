e42f46718b1ea08d0249b66592b38a2b860c2560
magic-akari <akari.ccino@gmail.com>
2023-09-17 18:39:54 +0800

    perf: reduce wasm size

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-set -Eeuo pipefail
+set -Eeo pipefail
 
 cd $(dirname $0)/..
 project_root=$(pwd)
@@ -16,7 +16,15 @@ ninja clang-format-wasm
 cd $project_root
 
 sed 's/output.instance.exports/(output.instance||output).exports/' build/clang-format-wasm.js >build/clang-format.js
-cp build/clang-format-wasm.wasm npm/clang-format.wasm
+
+if [[ ! -z "${WASM_OPT}" ]]; then
+    wasm-opt -Os build/clang-format-wasm.wasm -o build/clang-format-Os.wasm
+    wasm-opt -Oz build/clang-format-wasm.wasm -o build/clang-format-Oz.wasm
+fi
+
+SMALLEST_WASM=$(ls -Sr build/*.wasm | head -1)
+
+cp $SMALLEST_WASM npm/clang-format.wasm
 npm exec terser -- src/template.js build/clang-format.js --config-file .terser.json --output npm/clang-format.js
 
 cp src/clang-format.d.ts src/vite.js npm
